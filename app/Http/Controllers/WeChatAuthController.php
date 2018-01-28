ea296a8c82cd861461afadb205c63b80087fec5a
GuoHeng <guoheng@qiyi.com>
2018-01-28 15:25:05 +0800

    update

--- app/Http/Controllers/WeChatAuthController.php
+++ app/Http/Controllers/WeChatAuthController.php
@@ -22,8 +22,6 @@ class WeChatAuthController extends Controller
         $account        = new Account($this->originalId);
         $weChatUserInfo = new UserInfo($account);
         $simpleInfo     = $weChatUserInfo->getAuthAccessToken($code);
-        echo "<pre>";
-        var_dump($simpleInfo);
         $weChatUserInfo->storeAuthAccessToken($simpleInfo['access_token']);
         $weChatUserInfo->storeRefreshToken($simpleInfo['refresh_token']);
 
@@ -32,25 +30,34 @@ class WeChatAuthController extends Controller
         //判断用户是否存在
         $userExists = WeChatUserInfoModel::where('open_id', $openId)
             ->where('original_id', $account->getOriginalId())->get()->toArray();
-        var_dump($userExists);
         if (!$userExists) {
             $info               = $weChatUserInfo->getUserInfoByToken($simpleInfo['access_token'], $openId);
-            var_dump($info);
             $model              = new WeChatUserInfoModel();
             $model->open_id     = $openId;
             $model->original_id = $account->getOriginalId();
             $model->status      = 0;
-            $model->nickname    = isset($info['nickname']) ? $info['nickname'] : '';
+            $model->nickname    = $info['nickname'];
             $model->sex         = isset($info['sex']) ? $info['sex'] : 0;
             $model->province    = isset($info['province']) ? $info['province'] : '';
             $model->city        = isset($info['city']) ? $info['city'] : '';
             $model->country     = isset($info['country']) ? $info['country'] : '';
             $model->headimgurl  = isset($info['headimgurl']) ? $info['headimgurl'] : '';
             $model->unionid     = isset($info['unionid']) ? $info['unionid'] : '';
-            $saveResult = $model->save();
-            $saveStatus = $saveResult ? true : false;
-        } else {
-            var_dump($userExists);
+            $saveResult         = $model->save();
+            $saveStatus         = $saveResult ? true : false;
+        } else if (empty($userExists['nickname'])) {
+            $info = $weChatUserInfo->getUserInfoByToken($simpleInfo['access_token'], $openId);
+            WeChatUserInfoModel::where('open_id', $openId)
+                ->where('original_id', $account->getOriginalId())
+                ->update([
+                    'nickname'   => $info['nickname'],
+                    'sex'        => isset($info['sex']) ? $info['sex'] : 0,
+                    'province'   => isset($info['province']) ? $info['province'] : '',
+                    'city'       => isset($info['city']) ? $info['city'] : '',
+                    'country'    => isset($info['country']) ? $info['country'] : '',
+                    'headimgurl' => isset($info['headimgurl']) ? $info['headimgurl'] : '',
+                    'unionid'    => isset($info['unionid']) ? $info['unionid'] : ''
+                ]);
         }
 
 //        $a = Redis::get('a');
