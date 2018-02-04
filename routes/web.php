e5ebf361c5bd9ca07401341988023bada3641d95
GuoHeng <guoheng@qiyi.com>
2018-02-04 11:16:43 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/project/create', 'ProjectController@create');
 Route::get('/project/update', 'ProjectController@update');
 Route::get('/project/delete', 'ProjectController@delete');
 
-Route::post('/wechat', 'WeChatDockingController@index');
+Route::any('/wechat', 'WeChatDockingController@index');
 Route::get('/wechatAuth', 'WeChatAuthController@index');
