2767f813b87794814134482d6cd537b5a5e60366
GuoHeng <guoheng@qiyi.com>
2018-02-07 17:18:41 +0800

    update

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UploadController extends Controller
+{
+    public function image(Request $request)
+    {
+        return response()->json('asdf');
+    }
+}
---
+++ resources/views/album/uploadImage.blade.php
@@ -0,0 +1,28 @@
+<!-- Page-->
+<div class="page">
+    <div class="fab fab-right-bottom fab-morph" data-morph-to=".toolbar"><a href="#"><i class="icon f7-icons ios-only">add</i><i class="icon f7-icons ios-only">close</i><i class="icon material-icons md-only">add</i><i class="icon material-icons md-only">close</i></a></div>
+    <!-- Navbar-->
+    <div class="navbar">
+        <div class="navbar-inner">
+            <div class="center">Floating Action Button</div>
+        </div>
+    </div>
+    <!-- Floating Action Button -->
+    <div class="fab fab-right-bottom">
+        <a href="#">
+            <i class="icon f7-icons">add</i>
+        </a>
+    </div>
+    <!-- Another Floating Action Button -->
+    <div class="fab fab-left-bottom">
+        <a href="#">
+            <i class="icon f7-icons">add</i>
+        </a>
+    </div>
+    <!-- Scrollable Page Content-->
+    <div class="page-content">
+        <div class="block">
+            Lorem ipsum dolor sit amet, ....
+        </div>
+    </div>
+</div>
